f30ce36e16317af356a164112c16838658fd6ffc
UkloF <sirasit.volk@gmail.com>
2025-12-04 19:07:19 +0700

    camp-HTML-FORM

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ส่วนหัว HTML</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style>
+        body{
+            font-family: "Prompt", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+    </style>
+</head>
+<body>
+    <div class="container mt-4">
+        <h1>Workshop #HTML - FORM</h1>
+        <form>
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <label for="fname">ชื่อ</label>
+                </div>
+                <div class="col">
+                    <input id="fname" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <label for="lname">สกุล</label>
+                </div>
+                <div class="col">
+                    <input id="lname" class="form-control">
+                </div>
+            </div>
+        </form>
+    </div>
+    <!-- <style>
+        h1 {color: red;}
+    </style>
+    <h1 id="" class="">&lt;HTML 101&gt;</h1>
+    <h2>HTML 101</h2>
+    <h3>HTML 101</h3>
+    <hr />
+    <hr>
+    <table border="1" width="100%">
+        
+        <thead>
+            <tr>
+                <th>ลำดับ</th>
+                <th>ชื่อ</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>i</td>
+                <td>sus</td>
+            </tr>
+            <tr>
+                <td>asdasdasd</td>
+            </tr>
+        </tbody>
+    </table> -->
+</body>
+</html>
