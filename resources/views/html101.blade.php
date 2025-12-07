5f4ce5b96f0307c573916d3dad67b9306ce20d0c
UkloF <sirasit.volk@gmail.com>
2025-12-08 00:26:40 +0700

    camp-HTML-form-2

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -8,27 +8,22 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <style>
-        body{
-            font-family: "Prompt", sans-serif;
-            font-weight: 400;
-            font-style: normal;
-        }
-    </style>
+    
 </head>
 <body>
-    <div class="container mt-4">
+    <div class="container mt-4 mb-4">
         <h1>Workshop #HTML - FORM</h1>
-        <form>
-            <div class="row">
+        <form class=>
+            <div class="row mt-1">
                 <div class="col-sm-12 col-md-4">
                     <label for="fname">ชื่อ</label>
                 </div>
+                
                 <div class="col">
                     <input id="fname" class="form-control">
                 </div>
             </div>
-            <div class="row">
+            <div class="row mt-1">
                 <div class="col-sm-12 col-md-4">
                     <label for="lname">สกุล</label>
                 </div>
@@ -36,33 +31,41 @@
                     <input id="lname" class="form-control">
                 </div>
             </div>
+            <div class="row mt-1">
+                <div class="col-sm-12 col-md-4">
+                    <label for="Bday">วัน/เดือน/ปีเกิด</label>
+                </div>
+                <div class="col">
+                    <input type="date" id="Bday" class="form-control">
+                </div>
+            </div>
+            <div class="row mt-1">
+                <div class="col-sm-12 col-md-4">
+                    <label for="age">อายุ</label>
+                </div>
+                <div class="col">
+                    <input id="age" class="form-control">
+                </div>
+            </div>
         </form>
     </div>
-    <!-- <style>
-        h1 {color: red;}
-    </style>
-    <h1 id="" class="">&lt;HTML 101&gt;</h1>
-    <h2>HTML 101</h2>
-    <h3>HTML 101</h3>
-    <hr />
-    <hr>
-    <table border="1" width="100%">
-        
-        <thead>
-            <tr>
-                <th>ลำดับ</th>
-                <th>ชื่อ</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>i</td>
-                <td>sus</td>
-            </tr>
-            <tr>
-                <td>asdasdasd</td>
-            </tr>
-        </tbody>
-    </table> -->
 </body>
+    <style>
+        body{
+            font-family: "Prompt", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+            background: rgb(212, 204, 187);
+        
+        }
+        .container{
+            background: rgb(183, 175, 149);
+            width: 500px;
+            border-radius: 15px;
+            padding: 30px 40px;
+        }
+        h1{
+            font-size: 32px;
+        }
+    </style>
 </html>
