20c5aa3c813392e35227ed0371f3d6a0d4f314af
UkloF <sirasit.volk@gmail.com>
2025-12-10 15:01:23 +0700

    camp-HTML-form-3

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -11,19 +11,19 @@
     
 </head>
 <body>
-    <div class="container mt-4 mb-4">
+    <div class="container mt-4 mb-4 ms-8">
         <h1>Workshop #HTML - FORM</h1>
         <form class=>
-            <div class="row mt-1">
+            <div class="row">
                 <div class="col-sm-12 col-md-4">
                     <label for="fname">ชื่อ</label>
                 </div>
-                
                 <div class="col">
                     <input id="fname" class="form-control">
                 </div>
             </div>
-            <div class="row mt-1">
+
+            <div class="row mt-2">
                 <div class="col-sm-12 col-md-4">
                     <label for="lname">สกุล</label>
                 </div>
@@ -31,7 +31,8 @@
                     <input id="lname" class="form-control">
                 </div>
             </div>
-            <div class="row mt-1">
+
+            <div class="row mt-2">
                 <div class="col-sm-12 col-md-4">
                     <label for="Bday">วัน/เดือน/ปีเกิด</label>
                 </div>
@@ -39,12 +40,107 @@
                     <input type="date" id="Bday" class="form-control">
                 </div>
             </div>
-            <div class="row mt-1">
+
+            <div class="row mt-2">
                 <div class="col-sm-12 col-md-4">
                     <label for="age">อายุ</label>
                 </div>
                 <div class="col">
                     <input id="age" class="form-control">
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-sm-12 col-md-4">
+                    <label>เพศ</label>
+                </div>
+                <div class="col-md-2">
+                    <input class="form-check-input" type="radio" name="phed" id="radioDefault1">
+                    <label class="form-check-label" for="radioDefault1">
+                        ชาย
+                    </label>
+                </div>
+                <div class="col">
+                    <input class="form-check-input" type="radio" name="phed" id="radioDefault2">
+                    <label class="form-check-label" for="radioDefault2">
+                        หญิง
+                    </label>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-sm-12 col-md-4">
+                    <label>รูป</label>
+                </div>
+                <div class="col">
+                    <input class="form-control form-control-sm" id="roop" type="file">
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-sm-12 col-md-4">
+                    <label>ที่อยู่</label>
+                </div>
+                <div class="col">
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                        <label for="floatingTextarea2">ที่อยู่</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-sm-12 col-md-4">
+                    <label for="shee">สี</label>
+                </div>
+                <div class="col-md-8">
+                    <select class="form-select" aria-label="Default select example" id="shee">
+                        <option selected>สีที่ชอบ</option>
+                        <option value="1">แดง</option>
+                        <option value="2">ดำ</option>
+                        <option value="3">ขาว</option>
+                        <option value="4">ชมพู</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-sm-12 col-md-4">
+                    <label>แนวเพลงที่ชอบ</label>
+                </div>
+                <div class="col-md-2">
+                    <input class="form-check-input" type="radio" name="pleang" id="pleang1">
+                    <label class="form-check-label" for="pleang1">
+                        ป๊อป
+                    </label>
+                </div>
+                <div class="col-md-2">
+                    <input class="form-check-input" type="radio" name="pleang" id="pleang2">
+                    <label class="form-check-label" for="pleang2">
+                        ร๊อค
+                    </label>
+                </div>
+                <div class="col-md-3">
+                    <input class="form-check-input" type="radio" name="pleang" id="pleang3">
+                    <label class="form-check-label" for="pleang3">
+                        ลูกทุ่ง
+                    </label>
+                </div>
+            </div>
+            
+            <div class="form-check mt-4">
+                <input class="form-check-input" type="checkbox" value="" id="yinyom">
+                <label class="form-check-label" for="yinyom">
+                    ยินยอมให้เก็บข้อมูล
+                </label>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-md-8">
+                    <button type="reset" class="btn btn-danger">Reset</button>
+                </div>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-success">Submit</button>
                 </div>
             </div>
         </form>
@@ -56,7 +152,9 @@
             font-weight: 400;
             font-style: normal;
             background: rgb(212, 204, 187);
-        
+            font-size: 14px;
+            /* background-image: url('https://i.redd.it/00gl7374rjmf1.jpeg'); */
+            
         }
         .container{
             background: rgb(183, 175, 149);
@@ -67,5 +165,6 @@
         h1{
             font-size: 32px;
         }
+        
     </style>
 </html>
